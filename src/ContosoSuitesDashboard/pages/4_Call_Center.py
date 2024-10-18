00fd08fe89341248081ffae1b55ff8f3abc80b4f
Yuvaraj <yuvaraj.kesavan@hotmail.com>
2024-10-18 21:42:50 +0000

    Added sppech to text and translate vector embedding store in to cosmos db

--- src/ContosoSuitesDashboard/pages/4_Call_Center.py
+++ src/ContosoSuitesDashboard/pages/4_Call_Center.py
@@ -53,10 +53,25 @@ def create_transcription_request(audio_file, speech_recognition_language="en-US"
         done= True
 
     # TODO: Subscribe to the events fired by the conversation transcriber
+    transcriber.transcribed.connect(handle_final_result)
+    transcriber.session_started.connect(lambda evt: print(f'SESSION STARTED: {evt}'))
+    transcriber.session_stopped.connect(lambda evt: print(f'SESSION STOPPED {evt}'))
+    transcriber.canceled.connect(lambda evt: print(f'CANCELED {evt}'))
+
     # TODO: stop continuous transcription on either session stopped or canceled events
+    transcriber.session_stopped.connect(stop_cb)
+    transcriber.canceled.connect(stop_cb)
+
+    transcriber.start_transcribing_async()
 
     # TODO: remove this placeholder code and perform the actual transcription
-    all_results = ['This is a test.', 'Fill in with real transcription.']
+    _, wav_data = wavfile.read(audio_file)
+    stream.write(wav_data.tobytes())
+    stream.close()
+    while not done:
+        time.sleep(.5)
+
+    transcriber.stop_transcribing_async()
 
     return all_results
 
@@ -86,7 +101,32 @@ def make_azure_openai_chat_request(system, call_contents):
 def is_call_in_compliance(call_contents, include_recording_message, is_relevant_to_topic):
     """Analyze a call for relevance and compliance."""
 
-    return "This is a placeholder result. Fill in with real compliance analysis."
+    joined_call_contents = ' '.join(call_contents)
+    if include_recording_message:
+        include_recording_message_text = "2. Was the caller aware that the call was being recorded?"
+    else:
+        include_recording_message_text = ""
+
+    if is_relevant_to_topic:
+        is_relevant_to_topic_text = "3. Was the call relevant to the hotel and resort industry?"
+    else:
+        is_relevant_to_topic_text = ""
+
+    system = f"""
+        You are an automated analysis system for Contoso Suites.
+        Contoso Suites is a luxury hotel and resort chain with locations
+        in a variety of Caribbean nations and territories.
+
+        You are analyzing a call for relevance and compliance.
+
+        You will only answer the following questions based on the call contents:
+        1. Was there vulgarity on the call?
+        {include_recording_message_text}
+        {is_relevant_to_topic_text}
+    """
+
+    response = make_azure_openai_chat_request(system, joined_call_contents)
+    return response.choices[0].message.content
 
 @st.cache_data
 def generate_extractive_summary(call_contents):
@@ -100,7 +140,29 @@ def generate_extractive_summary(call_contents):
     # Join them together with spaces to pass in as a single document.
     joined_call_contents = ' '.join(call_contents)
 
-    return "This is a placeholder result. Fill in with real extractive summary."
+    # Create a TextAnalyticsClient, connecting it to your Language Service endpoint.
+    client = TextAnalyticsClient(language_endpoint, AzureKeyCredential(language_key))
+    
+    # Call the begin_analyze_actions method on your client, passing in the joined
+    # call_contents as an array and an ExtractiveSummaryAction with a max_sentence_countof 2.
+    poller = client.begin_analyze_actions(
+        [joined_call_contents],
+        actions = [
+           ExtractiveSummaryAction(max_sentence_count=2)
+        ]
+    )
+
+    # Extract the summary sentences and merge them into a single summary string.
+    for result in poller.result():
+        summary_result = result[0]
+        if summary_result.is_error:
+            st.error(f'Extractive summary resulted in an error with code "{summary_result.code}" and message "{summary_result.message}"')
+            return ''
+        
+        extractive_summary = " ".join([sentence.text for sentence in summary_result.sentences])
+
+    # Return the summary as a JSON object in the shape '{"call-summary":extractive_summary}'
+    return json.loads('{"call-summary":"' + extractive_summary + '"}')
 
 @st.cache_data
 def generate_abstractive_summary(call_contents):
@@ -114,7 +176,30 @@ def generate_abstractive_summary(call_contents):
     # Join them together with spaces to pass in as a single document.
     joined_call_contents = ' '.join(call_contents)
 
-    return "This is a placeholder result. Fill in with real abstractive summary."
+    # Create a TextAnalyticsClient, connecting it to your Language Service endpoint.
+    client = TextAnalyticsClient(language_endpoint, AzureKeyCredential(language_key))
+
+    # Call the begin_analyze_actions method on your client,
+    # passing in the joined call_contents as an array
+    # and an AbstractiveSummaryAction with a sentence_count of 2.
+    poller = client.begin_analyze_actions(
+        [joined_call_contents],
+        actions = [
+            AbstractiveSummaryAction(sentence_count=2)
+        ]
+    )
+
+    # Extract the summary sentences and merge them into a single summary string.
+    for result in poller.result():
+        summary_result = result[0]
+        if summary_result.is_error:
+            st.error(f'...Is an error with code "{summary_result.code}" and message "{summary_result.message}"')
+            return ''
+    
+        abstractive_summary = " ".join([summary.text for summary in summary_result.summaries])
+
+    # Return the summary as a JSON object in the shape '{"call-summary":abstractive_summary}'
+    return json.loads('{"call-summary":"' + abstractive_summary + '"}')
 
 @st.cache_data
 def generate_query_based_summary(call_contents):
@@ -124,7 +209,23 @@ def generate_query_based_summary(call_contents):
     # Join them together with spaces to pass in as a single document.
     joined_call_contents = ' '.join(call_contents)
 
-    return "This is a placeholder result. Fill in with real query-based summary."
+    # Write a system prompt that instructs the large language model to:
+    #    - Generate a short (5 word) summary from the call transcript.
+    #    - Create a two-sentence summary of the call transcript.
+    #    - Output the response in JSON format, with the short summary
+    #       labeled 'call-title' and the longer summary labeled 'call-summary.'
+    system = """
+        Write a five-word summary and label it as call-title.
+        Write a two-sentence summary and label it as call-summary.
+    
+        Output the results in JSON format.
+    """
+
+    # Call make_azure_openai_chat_request().
+    response = make_azure_openai_chat_request(system, joined_call_contents)
+
+    # Return the summary.
+    return response.choices[0].message.content   
 
 @st.cache_data
 def create_sentiment_analysis_and_opinion_mining_request(call_contents):
@@ -138,13 +239,127 @@ def create_sentiment_analysis_and_opinion_mining_request(call_contents):
     # Join them together with spaces to pass in as a single document.
     joined_call_contents = ' '.join(call_contents)
 
-    return "This is a placeholder result. Fill in with real sentiment analysis."
+    # Create a Text Analytics Client
+    client = TextAnalyticsClient(language_endpoint, AzureKeyCredential(language_key))
+
+    # Analyze sentiment of call transcript, enabling opinion mining.
+    result = client.analyze_sentiment([joined_call_contents], show_opinion_mining=True)
+
+    # Retrieve all document results that are not an error.
+    doc_result = [doc for doc in result if not doc.is_error]
+
+    # The output format is a JSON document with the shape:
+    # {
+    #     "sentiment": document_sentiment,
+    #     "sentiment-scores": {
+    #         "positive": document_positive_score_as_two_decimal_float,
+    #         "neutral": document_neutral_score_as_two_decimal_float,
+    #         "negative": document_negative_score_as_two_decimal_float
+    #     },
+    #     "sentences": [
+    #         {
+    #             "text": sentence_text,
+    #             "sentiment": document_sentiment,
+    #             "sentiment-scores": {
+    #                 "positive": document_positive_score_as_two_decimal_float,
+    #                 "neutral": document_neutral_score_as_two_decimal_float,
+    #                 "negative": document_negative_score_as_two_decimal_float
+    #             },
+    #             "mined_opinions": [
+    #                 {
+    #                     "target-sentiment": opinion_sentiment,
+    #                     "target-text": opinion_target,
+    #                     "target-scores": {
+    #                         "positive": document_positive_score_as_two_decimal_float,
+    #                         "neutral": document_neutral_score_as_two_decimal_float,
+    #                         "negative": document_negative_score_as_two_decimal_float
+    #                     },
+    #                     "assessments": [
+    #                       {
+    #                         "assessment-sentiment": assessment_sentiment,
+    #                         "assessment-text": assessment_text,
+    #                         "assessment-scores": {
+    #                             "positive": document_positive_score_as_two_decimal_float,
+    #                             "negative": document_negative_score_as_two_decimal_float
+    #                         }
+    #                       }
+    #                     ]
+    #                 }
+    #             ]
+    #         }
+    #     ]
+    # }
+    sentiment = {}
+
+    # Assign the correct values to the JSON object.
+    for document in doc_result:
+        sentiment["sentiment"] = document.sentiment
+        sentiment["sentiment-scores"] = {
+            "positive": document.confidence_scores.positive,
+            "neutral": document.confidence_scores.neutral,
+            "negative": document.confidence_scores.negative
+        }
+
+        sentences = []
+        for s in document.sentences:
+            sentence = {}
+            sentence["text"] = s.text
+            sentence["sentiment"] = s.sentiment
+            sentence["sentiment-scores"] = {
+                "positive": s.confidence_scores.positive,
+                "neutral": s.confidence_scores.neutral,
+                "negative": s.confidence_scores.negative
+            }
+
+            mined_opinions = []
+            for mined_opinion in s.mined_opinions:
+                opinion = {}
+                opinion["target-text"] = mined_opinion.target.text
+                opinion["target-sentiment"] = mined_opinion.target.sentiment
+                opinion["sentiment-scores"] = {
+                    "positive": mined_opinion.target.confidence_scores.positive,
+                    "negative": mined_opinion.target.confidence_scores.negative,
+                }
+
+                opinion_assessments = []
+                for assessment in mined_opinion.assessments:
+                    opinion_assessment = {}
+                    opinion_assessment["text"] = assessment.text
+                    opinion_assessment["sentiment"] = assessment.sentiment
+                    opinion_assessment["sentiment-scores"] = {
+                        "positive": assessment.confidence_scores.positive,
+                        "negative": assessment.confidence_scores.negative
+                    }
+                    opinion_assessments.append(opinion_assessment)
+
+                opinion["assessments"] = opinion_assessments
+                mined_opinions.append(opinion)
+
+            sentence["mined_opinions"] = mined_opinions
+            sentences.append(sentence)
+
+        sentiment["sentences"] = sentences
+
+    return sentiment
 
 def make_azure_openai_embedding_request(text):
     """Create and return a new embedding request. Key assumptions:
     - Azure OpenAI endpoint, key, and deployment name stored in Streamlit secrets."""
 
-    return "This is a placeholder result. Fill in with real embedding."
+    aoai_endpoint = st.secrets["aoai"]["endpoint"]
+    aoai_key = st.secrets["aoai"]["key"]
+    aoai_embedding_deployment_name = st.secrets["aoai"]["embedding_deployment_name"]
+
+    client = openai.AzureOpenAI(
+        api_key=aoai_key,
+        api_version="2024-06-01",
+        azure_endpoint = aoai_endpoint
+    )
+    # Create and return a new embedding request
+    return client.embeddings.create(
+        model=aoai_embedding_deployment_name,
+        input=text
+    )
 
 def normalize_text(s):
     """Normalize text for tokenization."""
@@ -165,10 +380,12 @@ def generate_embeddings_for_call_contents(call_contents):
     - Azure OpenAI endpoint, key, and deployment name stored in Streamlit secrets."""
 
     # Normalize the text for tokenization
-    # Call make_azure_openai_embedding_request() with the normalized content
-    # Return the embeddings
+    normalized_content = normalize_text(call_contents)
 
-    return [0, 0, 0]
+    # Call make_azure_openai_embedding_request() with the normalized content
+    response = make_azure_openai_embedding_request(normalized_content)
+
+    return response.data[0].embedding
 
 def save_transcript_to_cosmos_db(transcript_item):
     """Save embeddings to Cosmos DB vector store. Key assumptions:
@@ -181,9 +398,19 @@ def save_transcript_to_cosmos_db(transcript_item):
     cosmos_database_name = st.secrets["cosmos"]["database_name"]
     cosmos_container_name = "CallTranscripts"
 
+    print(cosmos_endpoint)
+    print(cosmos_key)
+    print(cosmos_database_name)
+    print(cosmos_container_name)
+    
     # Create a CosmosClient
+    client = CosmosClient(url=cosmos_endpoint, credential=cosmos_key)
     # Load the Cosmos database and container
+    database = client.get_database_client(cosmos_database_name)
+    container = database.get_container_client(cosmos_container_name)
+
     # Insert the call transcript
+    container.create_item(body=transcript_item)
 
 ####################### HELPER FUNCTIONS FOR MAIN() #######################
 def perform_audio_transcription(uploaded_file):
